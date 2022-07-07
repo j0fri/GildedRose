e32c80b581966bdf1f99c1beef256b9fa55f7171
Jordi Frittoli <Jordi.Frittoli@softwire.com>
2022-07-07 12:02:45 +0100

    Added ItemType 'enum'

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,43 +1,52 @@
 # -*- coding: utf-8 -*-
 
-
-
 class GildedRose(object):
 
     def __init__(self, items):
         self.items = items
 
     def update_quality(self):
+
+        class ItemType:
+            general = 0
+            brie = 1
+            sulfuras = 2
+            backstage_pass = 3
+            conjured = 4
+
         for item in self.items:
-            if item.name.startswith("Conjured"):     # Handles the cases where items can
-                item_type = "Conjured"               # start with a name, rather than have a name
+            if item.name == "Aged Brie":
+                item_type = ItemType.brie
+            elif item.name.startswith("Conjured"):     # Handles the cases where items can
+                item_type = ItemType.conjured          # start with a name, rather than have a name
             elif item.name.startswith("Sulfuras"):
-                item_type = "Sulfuras"
+                item_type = ItemType.sulfuras
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                item_type = ItemType.backstage_pass
             else:
-                item_type = item.name
+                item_type = ItemType.general
 
             change = -1 if item.sell_in > 0 else -2
             match item_type:
-                case "Aged Brie":
+                case ItemType.brie:
                     change *= -1
-                case "Sulfuras":
+                case ItemType.sulfuras:
                     change = 0
-                case "Backstage passes to a TAFKAL80ETC concert":
+                case ItemType.backstage_pass:
                     if item.sell_in > 10:
                         change *= -1
                     elif 5 < item.sell_in:    # 5 < sell_in <= 10
                         change *= -2
-                        print("Double")
                     elif item.sell_in > 0:
                         change *= -3
                     else:
                         change = 0        # Quality goes to 0 once concert has passed
                         item.quality = 0
 
-                case "Conjured":
+                case ItemType.conjured:
                     change *= 2
 
-            if item_type != "Sulfuras":
+            if item_type != ItemType.sulfuras:
                 item.sell_in -= 1
 
             item.quality = min(50, max(0, item.quality + change))
